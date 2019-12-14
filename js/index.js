b6489a3a3529fca419a8d917f917573d5bcaa6dc
Jorge Fabregas <jorge.fabregas22@gmail.com>
2019-12-13 23:06:34 -0500

    arrays

--- js/index.js
+++ js/index.js
@@ -4,3 +4,25 @@ var nav = document.getElementById ('nav');
 btnMenu.addEventListener ('click', function () {
     nav.classList.toggle ('showMenu');
 })
+var allSmartphones = [
+    {
+        brand : 'Apple',
+        model : 'iPhone 11',
+        capacity : '64 GB',
+        display : '5.5"',
+        price : '1799.99'
+    },
+    {
+        brand : 'Apple',
+        model : 'iPhone 11',
+        capacity : '128 GB',
+        display : '5.5"',
+        price : '1799.99'
+    }
+]
+
+var newSmart = allSmartphones.filter (function (element) {
+    return (element.price === '1799.99')
+});
+
+console.log (newSmart);
