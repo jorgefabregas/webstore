e2f896a566d0d61b565476939029129c5feb40fa
Jorge Fabregas <jorge.fabregas22@gmail.com>
2019-12-16 03:06:20 -0500

    add articles

--- js/index.js
+++ js/index.js
@@ -4,25 +4,35 @@ var nav = document.getElementById ('nav');
 btnMenu.addEventListener ('click', function () {
     nav.classList.toggle ('showMenu');
 })
-var allSmartphones = [
+const allSmartphones = [
     {
-        brand : 'Apple',
-        model : 'iPhone 11',
-        capacity : '64 GB',
-        display : '5.5"',
-        price : '1799.99'
-    },
-    {
-        brand : 'Apple',
-        model : 'iPhone 11',
-        capacity : '128 GB',
-        display : '5.5"',
-        price : '1799.99'
-    }
-]
-
-var newSmart = allSmartphones.filter (function (element) {
-    return (element.price === '1799.99')
-});
-
-console.log (newSmart);
+      brand: `Samsung`,
+      model: `Galaxy Note 10`,
+      capacity: `256 GB`,
+      display: '5.5"',
+      color: 'black',
+      code: 'SM-01',
+    },{
+        brand: `Samsung`,
+        model: `Galaxy S10`,
+        capacity: `128 GB`,
+        display: '5.5"',
+        color: 'black',
+        code: 'SM-02',
+      },{
+        brand: `Apple`,
+        model: `iPhone X`,
+        capacity: `256 GB`,
+        display: '5.5"',
+        color: 'black',
+        code: 'SM-03',
+      },{
+        brand: `Apple`,
+        model: `iPhone 11`,
+        capacity: `64 GB`,
+        display: '5.5"',
+        color: 'black',
+        code: 'SM-04',
+      },
+  ];
+    
